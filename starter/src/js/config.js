1b07fe3e31fdc7428c3a4b3b232d920b7c158e28
rjsausnitis <rjsausnitis@gmail.com>
2024-06-06 23:03:09 +0200

    Initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes'; // all caps because it's a contant that will never change
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c6b7b57c-9bf4-4dc9-8663-1c0ebbcf8dd2';
+export const MODAL_CLOSE_SEC = 2.5;
